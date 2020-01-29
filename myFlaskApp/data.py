bed516329ae9fea756667729870db7040445dce9
Piotrtat <piotr.tatarski85@gmail.com>
2020-01-29 23:34:02 +0100

    articles

---
+++ myFlaskApp/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet sapien mauris sit amet tempor.',
+            'author': 'Piotr Tatarski',
+            'create date': '01-29-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet sapien mauris sit amet tempor.',
+            'author': 'Adrian Kowalski',
+            'create date': '01-29-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet sapien mauris sit amet tempor.',
+            'author': 'Natalia Tatarska',
+            'create date': '01-29-2020'
+        }
+    ]
+    return articles
